b46b3ef0124d2d831acd262aefabaadd4a53f5d7
prakashbote11 <prakashbote1989@gmail.com>
2020-06-12 16:46:23 +0530

    srgdgdg

---
+++ CucumberProject/src/test/java/stepDefinations/stepDefination.java
@@ -0,0 +1,34 @@
+package stepDefinations;
+import org.junit.runner.RunWith;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class stepDefination {
+	
+	/*@Given("^User is on google search page$")
+	public void User_is_on_Google_Search_Page() throws Throwable{
+		
+		System.out.println("On gooogle search");
+			
+	}
+	
+	@When("^I have search string to enter$")
+	public void User_enters_the_search_string() throws Throwable {
+		
+		System.out.println("User entered the string");
+			
+	}
+	
+	@Then("^That specific page shoule be displayed$")
+	public void User_enter_and_hit_button() throws Throwable{
+		
+		System.out.println("user hit the enter button");
+			
+	}
+*/
+}
---
+++ CucumberProject/target/test-classes/features/google.feature
@@ -0,0 +1,6 @@
+Feature: Google Search
+
+Scenario: Insert a specific string and press enter
+Given User is on google search page
+When I have search string to enter
+Then That specific page shoule be displayed
